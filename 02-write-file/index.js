d078fcaf27c02c752042798ac68e49f92b8f40cf
127zeppelin <127.zeppelin@gmail.com>
2023-05-08 17:21:01 +0300

    02-write-file done

---
+++ 02-write-file/index.js
@@ -0,0 +1,33 @@
+const fs = require('fs');
+const path = require('path');
+const filePath = path.join(__dirname, 'text.txt');
+
+
+if (fs.existsSync(filePath)) {
+  console.log('Файл уже существует');
+} else {
+  console.log('Файл не существует, создаем новый файл text.txt');
+  fs.writeFileSync(filePath, '');
+}
+
+process.on('SIGINT', () => {
+  stdout.write('Удачи в изучении Node.js!');
+  process.exit();
+});
+
+const { stdin, stdout } = process;
+stdin.on('data', data => {
+  const text = data.toString().trim(); 
+
+  if (text === 'exit') {
+    stdout.write('Удачи в изучении Node.js!');
+    process.exit();
+  } else {
+    fs.appendFile(filePath, text + '\n', (err) => {
+      if (err) throw err;
+      console.log(`Успешно добавлен текст:${text}`);
+    });
+  }
+});
+
+
