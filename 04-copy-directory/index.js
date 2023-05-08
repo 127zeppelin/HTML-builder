a8a26a6c2ec4d35724739a4468fee3f8eb5acb26
127zeppelin <127.zeppelin@gmail.com>
2023-05-09 02:15:31 +0300

    04-copy-directory done

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,11 +1,56 @@
 const fs = require('fs');
 const path = require('path');
-const dirPath = path.join(__dirname, 'text.txt');
+const oldDirPath = path.join(__dirname, 'files');
+const dirPath = path.join(__dirname, 'files-copy');
 
+fs.access(dirPath, fs.constants.F_OK, (err) => {
+  if (!err) {
+    fs.rm(dirPath, { recursive: true }, (err) => {
+      if (err) {
+        console.error(err);
+      } else {
+      }
+      fs.mkdir(dirPath, null, (err) => {
+        if (err) throw err;
+        fs.readdir(oldDirPath, { withFileTypes: true }, (err, files) => {
+          if (err) {
+            console.error(err);
+            return;
+          }
+          files.forEach(file => {
+            const src = path.join(oldDirPath, file.name);
+            const dest = path.join(dirPath, file.name);
+        
+            fs.copyFile(src, dest, err => {
+              if (err) {
+                console.error(`Error copying file ${src} to ${dest}:`, err);
+              }
+            });
+          });
+        });
+      });
+    });
+  } else {
+    fs.mkdir(dirPath, null, (err) => {
+      if (err) throw err;
+      fs.readdir(oldDirPath, { withFileTypes: true }, (err, files) => {
+        if (err) {
+          console.error(err);
+          return;
+        }
+        files.forEach(file => {
+          const src = path.join(oldDirPath, file.name);
+          const dest = path.join(dirPath, file.name);
+      
+          fs.copyFile(src, dest, err => {
+            if (err) {
+              console.error(`Error copying file ${src} to ${dest}:`, err);
+            }
+          });
+        });
+      });
+    });
+  }
+ 
+});
 
-if (fs.existsSync(dirPath)) {
-  console.log('Файл уже существует');
-} else {
-  console.log('Файл не существует, создаем новый файл text.txt');
-  fs.writeFileSync(filePath, '');
-}
